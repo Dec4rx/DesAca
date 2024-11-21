2836874549c2aea39a2a61caaf263f3439ac2501
Dec4rx <decarwar@gmail.com>
2024-11-21 01:09:09 -0600

    Question GetAl
    -Ruta para obtener todas las preguntas

---
+++ src/main/java/com/DesAca/DesAca/Question/QuestionController.java
@@ -0,0 +1,26 @@
+package com.DesAca.DesAca.Question;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionController {
+    private final QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+    
+    @GetMapping()
+    public List<Question> getMethodName() {
+        return questionService.getAllQuestions();
+    }
+    
+}
--- src/main/java/com/DesAca/DesAca/Question/QuestionService.java
+++ src/main/java/com/DesAca/DesAca/Question/QuestionService.java
@@ -1,5 +1,17 @@
 package com.DesAca.DesAca.Question;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class QuestionService {
+    private final QuestionRepository questionRepository;
+    public List<Question> getAllQuestions(){
+        return questionRepository.findAll();
+    }
 
 }
